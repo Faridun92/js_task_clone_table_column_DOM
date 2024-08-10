c855d79fe0799fa906681d733f16b39ffa0db4ab
Mainaiev <farid.mainaiev@proton.me>
2024-08-10 17:53:17 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+// write your code here
+const table = document.querySelector("table");
+const headRows = table.tHead.rows;
+for (const row of headRows){
+    const position = row.cells[1].cloneNode(true);
+    row.insertBefore(position, row.cells[row.cells.length - 1]);
+}
+const bodyRows = table.tBodies[0].rows;
+for (const row of bodyRows){
+    const position = row.cells[1].cloneNode(true);
+    row.insertBefore(position, row.cells[row.cells.length - 1]);
+}
+const footRows = table.tFoot.rows;
+for (const row of footRows){
+    const position = row.cells[1].cloneNode(true);
+    row.insertBefore(position, row.cells[row.cells.length - 1]);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
